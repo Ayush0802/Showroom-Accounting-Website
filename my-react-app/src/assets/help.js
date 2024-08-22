288708d62eacf20cd1b090a22a22f57881e6dc8e
Ayush Bhut <112795950+Ayush0802@users.noreply.github.com>
2024-08-22 19:52:58 +0530

    Update help.js

--- my-react-app/src/assets/help.js
+++ my-react-app/src/assets/help.js
@@ -1,2 +1,2 @@
-// export const base_url = "https://space-interiors-app.vercel.app/"
-export const base_url = "http://localhost:5001"
+export const base_url = "https://myreactapp-server.vercel.app/"
+// export const base_url = "http://localhost:5001"
