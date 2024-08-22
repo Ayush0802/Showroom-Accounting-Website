5d380c959d8b23ef21c0b534dc3dea9d402efc37
Ayush Bhut <112795950+Ayush0802@users.noreply.github.com>
2024-08-22 19:56:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,11 +19,11 @@ const app = express()
 const port = process.env.PORT || 8000
 
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
 
-// app.use(cors({
-//     origin:"https://my-app-frontend-seven.vercel.app"
-// }))
+app.use(cors({
+    origin:"https://showroom-accounts-web.vercel.app"
+}))
 
 
 
