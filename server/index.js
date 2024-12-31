2f007078949edd43d684a5609113fb5cf5512a97
Ayush Bhut <112795950+Ayush0802@users.noreply.github.com>
2024-12-31 20:04:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,11 +21,6 @@ const port = process.env.PORT || 8000
 app.use(express.json())
 app.use(cors())
 
-app.use(cors({   
-    origin:"https://showroom-accounts-web.vercel.app"
-}))
-
-
 
 // Registration Page //
 
